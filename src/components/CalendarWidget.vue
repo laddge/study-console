eeaf58ff16d9f2c07a04c8848f6ba06afb520eae
Laddge <dev.laddge@gmail.com>
2023-03-28 04:41:33 +0000

    bug fix

--- src/components/CalendarWidget.vue
+++ src/components/CalendarWidget.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { getMonth, setMonth, getDate, startOfMonth, endOfMonth, getWeekOfMonth, startOfWeek, format } from 'date-fns'
+  import { ref } from 'vue'
+  import { addMonths, getDate, startOfMonth, endOfMonth, getWeekOfMonth, startOfWeek, format } from 'date-fns'
   import { ChevronLeftIcon, ChevronRightIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
 
-  const month = ref(0)
-
-  onMounted(() => {
-    month.value = getMonth(new Date())
-  })
+  const monthDiff = ref(0)
 
   const getCellDate = (row: number, col: number): Date => {
     const d = row * 7 + col
-    const cellDate = startOfWeek(startOfMonth(setMonth(new Date(), month.value)))
+    const cellDate = startOfWeek(addMonths(startOfMonth(new Date()), monthDiff.value))
     cellDate.setDate(getDate(cellDate) + d)
     return cellDate
   }
@@ -21,11 +17,11 @@
   <div>
     <div class="mx-3 mb-4 text-xl flex gap-2 items-center">
       <div class="grow font-bold">
-        {{ format(setMonth(new Date(), month), 'MMMM Y') }}
+        {{ format(addMonths(startOfMonth(new Date()), monthDiff), 'MMMM Y') }}
       </div>
-      <ArrowUturnLeftIcon v-if="month != getMonth(new Date())" @click="month = getMonth(new Date())" class="h-5 w-5 mr-3 text-indigo-500" />
-      <ChevronLeftIcon @click="month--" class="h-6 w-6 text-indigo-500" />
-      <ChevronRightIcon @click="month++" class="h-6 w-6 text-indigo-500" />
+      <ArrowUturnLeftIcon v-if="monthDiff != 0" @click="monthDiff = 0" class="h-5 w-5 mr-3 text-indigo-500" />
+      <ChevronLeftIcon @click="monthDiff--" class="h-6 w-6 text-indigo-500" />
+      <ChevronRightIcon @click="monthDiff++" class="h-6 w-6 text-indigo-500" />
     </div>
     <table class="table-fixed w-full text-center">
       <tr>
@@ -37,7 +33,7 @@
         <th>Fri</th>
         <th>Sat</th>
       </tr>
-      <tr v-for="(_, row) in getWeekOfMonth(endOfMonth(setMonth(new Date(), month)))" :key="row">
+      <tr v-for="(_, row) in getWeekOfMonth(endOfMonth(addMonths(startOfMonth(new Date()), monthDiff)))" :key="row">
         <td v-for="(_, col) in 7" :key="col" class="py-3">
           <div v-if="row == 0 && col == 0 || format(getCellDate(row, col), 'd') == '1'">
             {{ format(getCellDate(row, col), 'M/d') }}
