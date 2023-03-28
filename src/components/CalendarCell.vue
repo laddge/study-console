d67770629827f8cf9afc7c83ed59a0a1c622bfc2
Laddge <dev.laddge@gmail.com>
2023-03-28 07:34:36 +0000

    support holidays

--- src/components/CalendarCell.vue
+++ src/components/CalendarCell.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import { format, getDate } from 'date-fns'
+  import { format, getDate, isSunday } from 'date-fns'
+  import { isHoliday } from 'japanese-holidays'
 
   const props = defineProps<{ date: Date }>()
 </script>
@@ -7,7 +8,9 @@
 <template>
   <div class="h-16 p-1">
     <span :class="format(date, 'Y-M-d') == format(new Date(), 'Y-M-d') ? 'bg-indigo-100 rounded px-1.5 py-0.5' : 'px-1.5 py-0.5'">
-      <span v-if="getDate(props.date) == 1">{{ format(date, 'M/') }}</span>{{ getDate(props.date) }}
+      <span :class="isSunday(date) || isHoliday(date) ? 'text-red-600' : ''">
+        <span v-if="getDate(props.date) == 1">{{ format(date, 'M/') }}</span>{{ getDate(props.date) }}
+      </span>
     </span>
   </div>
 </template>
