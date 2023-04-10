a978c344c243863510f6feae0dd74273494af7b4
Laddge <dev.laddge@gmail.com>
2023-04-10 07:46:58 +0000

    ignore undefined for firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore, initializeFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +20,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize FireStore
+initializeFirestore(app, {
+  ignoreUndefinedProperties: true
+})
+
 // const analytics = getAnalytics(app);
 export const auth = getAuth(app)
 export const db = getFirestore(app);
